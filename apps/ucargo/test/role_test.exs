1d774ac6409284801f10ff7963c4bd1ce2a92cfa
Erik Vera <apcxiii@gmail.com>
2018-02-21 10:20:08 -0600

    added role test

---
+++ apps/ucargo/test/role_test.exs
@@ -0,0 +1,16 @@
+defmodule Ucargo.RoleTest do
+  @moduledoc """
+    Test File for drivers data layer
+  """
+  use Ucargo.DataCase
+
+  alias Ucargo.Role
+
+  test "Create Role" do
+
+    changeset = Role.changeset(%Role{}, 
+      %{name: "admin", 
+        admin: true})
+    assert changeset.valid? == true
+  end
+end
