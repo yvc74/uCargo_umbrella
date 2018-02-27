bc24663b3b656f5729d2d7b30be758e683e9992f
Xochitl Perez <xochitlperez@Xochitls-iMac.local>
2018-02-27 07:18:35 -0600

    Add test for drivers,custom brokers, orders

--- apps/ucargo/lib/mappers/ucargo_custom_brokers.ex
+++ apps/ucargo/lib/mappers/ucargo_custom_brokers.ex
@@ -3,11 +3,17 @@ defmodule Ucargo.CustomBroker do
   Ecto CustomBrokers Mapper
   """
   use Ecto.Schema
+  import Ecto.Changeset
+  alias Ucargo.CustomBroker
 
   schema "custom_brokers" do
     field :name, :string
     has_many :plannings, Ucargo.Planning
     many_to_many :drivers, Ucargo.Driver, join_through: "favourite_drivers"
   end
-
+  
+  def create_changeset(%CustomBroker{} = custombroker, attrs) do
+    custombroker
+      |> cast(attrs, [:name])
+  end
 end
