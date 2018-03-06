ebe78fe27faa5aea26fbceaf4abece8ea13adb34
misaelpc <misaelpcgm@gmail.com>
2018-03-05 20:32:14 -0600

    Update seed

--- apps/ucargo/priv/repo/seeds.exs
+++ apps/ucargo/priv/repo/seeds.exs
@@ -38,15 +38,28 @@ deliver_chgset = Delivery.create_changeset(delivery,
 order_with_pick = Ecto.Changeset.put_assoc(order_chs, :pickup, pick_chgset)
 order_with_delivery = Ecto.Changeset.put_assoc(order_with_pick, :delivery, deliver_chgset)
 
-
 order = Repo.insert! order_with_delivery
-pl_changeset = Planning.create_changeset(%Planning{}, %{})
-pl_with_order = Ecto.Changeset.put_assoc(pl_changeset, :order, order)
-Repo.insert! pl_with_order
 
 date_now = NaiveDateTime.utc_now()
-auction_params = Auction.create_changeset(%Auction{}, %{begin_date: date_now, end_date: NaiveDateTime.add(date_now, 86400, :second), ask_price: 10500.45 })
-pl_with_auction = Ecto.Changeset.put_assoc(pl_changeset, :auction, auction_params)
+
+auction_chgs = Auction.create_changeset(%Auction{},
+                   %{begin_date: date_now,
+                     end_date: NaiveDateTime.add(date_now, 86400, :second),
+                     ask_price: 10500.45})
+
+bid_chgs = Bid.create_changeset(%Bid{}, %{price: 324443, winner: true})
+auction_with_bids = Ecto.Changeset.put_assoc(auction_chgs, :bids, [bid_chgs])
+
+auction = Repo.insert! auction_with_bids
+
+pl_changeset = Planning.create_changeset(%Planning{}, %{})
+pl_with_order = Ecto.Changeset.put_assoc(pl_changeset, :order, order)
+pl_with_auction = Ecto.Changeset.put_assoc(pl_with_order, :auction, auction)
+
+
+#Repo.insert! pl_with_order
+
+
 Repo.insert! pl_with_auction
 
 # bid_params = Bid.create_changeset(%Bid{}, %{price: 324443, winner: true})
