938ec631024e6a55941dded92301816c7b62de6a
misaelpc <misaelpcgm@gmail.com>
2018-03-12 20:41:02 -0600

    Crea JS module for delivery Calendar

--- apps/ucargo_web/assets/js/launch.js
+++ apps/ucargo_web/assets/js/launch.js
@@ -1,9 +1,13 @@
 import {SetupCalendar} from "./setupcalendar.js"
+import {SetupDeliveryCalendar} from "./setupdeliverycalendar.js"
 
 class Launch {
   constructor() {
     this.setupcalendar = new SetupCalendar
     this.setupcalendar.configure()
+    this.setuppickupcalendar = new SetupDeliveryCalendar
+    this.setuppickupcalendar.configure()
+    
   }
 }
 
