b430278217ac26a32b1167a6e1952c28f1b034e0
misaelpc <misaelpcgm@gmail.com>
2018-03-24 13:43:49 -0600

    Update tests

--- apps/ucargo_web/test/support/order_insert.ex
+++ apps/ucargo_web/test/support/order_insert.ex
@@ -4,6 +4,7 @@ defmodule OrderInsertHelper do
   """
   alias Ucargo.Order
   alias Ucargo.Pickup
+  alias Ucargo.Custom
   alias Ucargo.Delivery
   alias Ucargo.Repo
 
@@ -15,6 +16,7 @@ defmodule OrderInsertHelper do
     order = %Order{}
     order_chs = Order.create_changeset(order, orders_params)
     pick_up = %Pickup{}
+    custom = %Custom{}
     delivery = %Delivery{} #32.5498703,-116.9378327
     pick_chgset = Pickup.create_changeset(pick_up,
             %{latitude: 32.5498703, longitude: -116.9378327,
@@ -26,9 +28,17 @@ defmodule OrderInsertHelper do
               name: "Plaza de toros Queretaro", 
               address: "Avenida Constituyentes, s/n, La Granja, 76190 Santiago de Querétaro, Qro.",
               schedule: "Lunes 9–15"})
+    custom_import_chgset = Custom.create_changeset(custom,
+            %{latitude: 32.5498703, longitude: -116.9378327,
+              name: "Aduana de Tijuana", 
+              address: "Garita Internacional, Perimetral Nte., 22430 Tijuana, B.C.",
+              schedule: "sábado	9–15",
+              responsible: "Benjamin Cedillo",
+              date: "2018-03-10"})
     order_with_pick = Ecto.Changeset.put_assoc(order_chs, :pickup, pick_chgset)
     order_with_delivery = Ecto.Changeset.put_assoc(order_with_pick, :delivery, deliver_chgset)
+    order_with_custom = Ecto.Changeset.put_assoc(order_with_delivery, :custom, custom_import_chgset)
 
-    Repo.insert! order_with_delivery
+    Repo.insert! order_with_custom
   end
 end
