403b7ad86e65eca72f559790073bfde19ea94713
misaelpc <misaelpcgm@gmail.com>
2018-03-27 13:20:56 -0600

    Render export and import after payment

--- apps/ucargo_web/lib/ucargo_web/controllers/assignment_controller.ex
+++ apps/ucargo_web/lib/ucargo_web/controllers/assignment_controller.ex
@@ -9,13 +9,17 @@ defmodule UcargoWeb.AssignmentController do
   def index(conn, _params) do
     resource = Guardian.Plug.current_resource(conn)
     broker = CustomBroker.fetch_plannings(resource)
-    render conn, "index.html", assigned_items: Order.fetch_assigned_items(broker.plannings)
+    render conn, "index.html",
+           assigned_items: Order.fetch_assigned_items(broker.plannings)
   end
 
   def new(conn, %{"bid_id" => bid_id, "planning_id" => planning_id}) do
     planning = Planning.find_by(:id, planning_id)
     bid = Bid.find_by(:id, bid_id)
-    order = Order.create_assignment(planning.order, bid.driver)
-    render conn, "index.html", planning: planning, order: order
+    Order.create_assignment(planning.order, bid.driver)
+    resource = Guardian.Plug.current_resource(conn)
+    broker = CustomBroker.fetch_plannings(resource)
+    render conn, "index.html",
+           assigned_items: Order.fetch_assigned_items(broker.plannings)
   end
 end
