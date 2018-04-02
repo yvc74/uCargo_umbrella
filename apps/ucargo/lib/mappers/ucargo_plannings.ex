851591c0bf1b7e961a4494f9b82f1bf062bab8a6
Xochitl Perez <xochitlperez@Xochitls-iMac.local>
2018-04-02 11:57:27 -0500

    add create new import and export ship

--- apps/ucargo/lib/mappers/ucargo_plannings.ex
+++ apps/ucargo/lib/mappers/ucargo_plannings.ex
@@ -40,12 +40,36 @@ defmodule Ucargo.Planning do
     Repo.one(query)
   end
 
-  def create_with_order(order_chs, pick_chgset, deliver_chgset, broker_id) do
-    order_with_pick = Ecto.Changeset.put_assoc(order_chs, :pickup, pick_chgset)
-    order_with_delivery = Ecto.Changeset.put_assoc(order_with_pick, :delivery, deliver_chgset)
+  def create_with_order_import(master_reference_chs, house_reference_chs, order_chs, custom_chgset, deliver_chgset, broker_id) do
+    order_with_custom = Ecto.Changeset.put_assoc(order_chs, :custom, custom_chgset)
+    order_with_delivery = Ecto.Changeset.put_assoc(order_with_custom, :delivery, deliver_chgset)
     order = Repo.insert! order_with_delivery
     notify_to_drivers(order)
-    pl_changeset = Planning.create_changeset(%Planning{}, %{custom_broker_id: broker_id})
+    pl_changeset = Planning.create_changeset(%Planning{}, %{master_reference: master_reference_chs, house_reference: house_reference_chs,custom_broker_id: broker_id})
+    date_now = NaiveDateTime.utc_now()
+    auction_chgs = Auction.create_changeset(%Auction{},
+                   %{begin_date: date_now,
+                     end_date: NaiveDateTime.add(date_now, 86400, :second),
+                     ask_price: 10500.45})
+
+    bid_chgs = Bid.create_changeset(%Bid{}, %{price: 324443, winner: true})
+    auction_with_bids = Ecto.Changeset.put_assoc(auction_chgs, :bids, [bid_chgs])
+
+    auction = Repo.insert! auction_with_bids
+    
+    pl_with_order = Ecto.Changeset.put_assoc(pl_changeset, :order, order)
+    pl_with_auction = Ecto.Changeset.put_assoc(pl_with_order, :auction, auction)
+    
+    Repo.insert! pl_with_auction
+  end
+
+  def create_with_order_export(master_reference_chs, house_reference_chs, order_chs, custom_chgset, pick_chgset, deliver_chgset, broker_id) do
+    order_with_custom = Ecto.Changeset.put_assoc(order_chs, :custom, custom_chgset)
+    order_with_delivery = Ecto.Changeset.put_assoc(order_with_custom, :delivery, deliver_chgset)
+    order_with_pick_up = Ecto.Changeset.put_assoc(order_with_delivery, :pickup, pick_chgset)
+    order = Repo.insert! order_with_pick_up
+    notify_to_drivers(order)
+    pl_changeset = Planning.create_changeset(%Planning{}, %{master_reference: master_reference_chs, house_reference: house_reference_chs,custom_broker_id: broker_id})
     date_now = NaiveDateTime.utc_now()
     auction_chgs = Auction.create_changeset(%Auction{},
                    %{begin_date: date_now,
--- apps/ucargo_web/lib/ucargo_web/controllers/ship_controller.ex
+++ apps/ucargo_web/lib/ucargo_web/controllers/ship_controller.ex
@@ -7,6 +7,7 @@ defmodule UcargoWeb.ShipController do
   alias Ucargo.Guardian
   alias Ucargo.CustomBroker
   alias Ucargo.Bid
+  alias Ucargo.Custom
   @export 1
   @import 0
 
@@ -51,27 +52,71 @@ defmodule UcargoWeb.ShipController do
 
   def new(conn, params) do
     broker = Guardian.Plug.current_resource(conn)
-    orders_params = %{score: 4, deadline: NaiveDateTime.utc_now(),
-                  status: "New", type: 1, distance: "350",
-                  merchandise_type: "Video", order_number: "47848",
-                  transport: "pick-up", weight: "800", comments: "None"}
+    %{"planning" => %{"master_reference" => master_reference_params, "house_reference" => house_reference_params, "order" => %{"delivery" => delivery_params, "custom" => custom_params} = order_params }} = params
     order = %Order{}
-    order_chs = Order.create_changeset(order, orders_params)
-    %{"planning" => %{"order" => %{"delivery" => delivery_params, "pickup" => pick_up_params}}} = params
-    pick_up = %Pickup{}
+    custom = %Custom{}
     delivery = %Delivery{}
-    pk_up_prms = pick_up_params
+    order_up_prms = order_params
+      |> Map.put("score", 4)
+      |> Map.put("distance", "500")
+      |> Map.put("order_number", "47848")
+      |> Map.put("deadline", NaiveDateTime.utc_now())
+    order_chs = Order.create_changeset(order, order_up_prms)
+
+    ct_up_prms = custom_params
       |> Map.put("latitude", 32.5498703)
       |> Map.put("longitude", -116.9378327)
+      |> Map.put("address", "puerto de veracruz soleadon")
 
     dvl_up_prms = delivery_params
       |> Map.put("latitude", 20.5848521)
       |> Map.put("longitude", -100.3965839)
       |> Map.put("name", "Centro Industrial Vallejo")
+      |> Map.put("address","#{delivery_params["street"]} #{delivery_params["ext"]} #{delivery_params["int"]}, #{delivery_params["zipcode"]}, #{delivery_params["neighborhood"]},#{delivery_params["delegation"]},#{delivery_params["city"]},#{delivery_params["state"]}")
 
-    pick_chgset = Pickup.create_changeset(pick_up, pk_up_prms)
+    cstm_chgset = Custom.create_changeset(custom, ct_up_prms)
     deliver_chgset = Delivery.create_changeset(delivery, dvl_up_prms)
-    Planning.create_with_order(order_chs, pick_chgset, deliver_chgset, broker.id)
+    Planning.create_with_order_import(master_reference_params, house_reference_params,order_chs, cstm_chgset, deliver_chgset, broker.id)
+    conn
+      |> redirect(to: "/plannings")
+  end
+
+  def new_export_ship(conn, params) do
+    broker = Guardian.Plug.current_resource(conn)
+    %{"planning" => %{"master_reference" => master_reference_params, "house_reference" => house_reference_params, "order" => %{"delivery" => delivery_params, "custom" => custom_params, "pickup" => pickup_params} = order_params }} = params
+    order = %Order{}
+    custom = %Custom{}
+    delivery = %Delivery{}
+    pickup = %Pickup{}
+    order_up_prms = order_params
+      |> Map.put("score", 4)
+      |> Map.put("distance", "500")
+      |> Map.put("order_number", "47848")
+      |> Map.put("deadline", NaiveDateTime.utc_now())
+    order_chs = Order.create_changeset(order, order_up_prms)
+
+    ct_up_prms = custom_params
+      |> Map.put("latitude", 32.5498703)
+      |> Map.put("longitude", -116.9378327)
+      |> Map.put("address", "puerto de veracruz soleadon")
+
+    dvl_up_prms = delivery_params
+      |> Map.put("latitude", 20.5848521)
+      |> Map.put("longitude", -100.3965839)
+      |> Map.put("name", "Centro Industrial Vallejo")
+      |> Map.put("address","#{delivery_params["street"]} #{delivery_params["ext"]} #{delivery_params["int"]}, #{delivery_params["zipcode"]}, #{delivery_params["neighborhood"]},#{delivery_params["delegation"]},#{delivery_params["city"]},#{delivery_params["state"]}")
+
+    pck_up_prms = pickup_params
+      |> Map.put("latitude", 20.5848521)
+      |> Map.put("longitude", -100.3965839)
+      |> Map.put("name", "Santa María la Rivera")
+      |> Map.put("address","#{pickup_params["street"]} #{pickup_params["ext"]} #{pickup_params["int"]}, #{pickup_params["zipcode"]}, #{pickup_params["neighborhood"]},#{pickup_params["delegation"]},#{pickup_params["city"]},#{pickup_params["state"]}")      
+    
+    cstm_chgset = Custom.create_changeset(custom, ct_up_prms)
+    deliver_chgset = Delivery.create_changeset(delivery, dvl_up_prms)
+    pck_chgset = Pickup.create_changeset(pickup, pck_up_prms)
+    
+    Planning.create_with_order_export(master_reference_params, house_reference_params,order_chs, cstm_chgset, pck_chgset, deliver_chgset, broker.id)
     conn
       |> redirect(to: "/plannings")
   end
