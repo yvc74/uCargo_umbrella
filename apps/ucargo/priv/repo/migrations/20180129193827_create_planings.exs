b99dc99b99e74d53baed54acf3dfffed08ed9ac3
Misael Pérez Chamorro <misaelperezchamorro@Misaels-MacBook-Pro.local>
2018-04-11 18:37:33 -0500

    Update assignments with bids winners

--- apps/ucargo/priv/repo/migrations/20180129193827_create_planings.exs
+++ apps/ucargo/priv/repo/migrations/20180129193827_create_planings.exs
@@ -6,6 +6,7 @@ defmodule Ucargo.Repo.Migrations.CreatePlanings do
       add :name, :string
       add :master_reference, :string
       add :house_reference, :string
+      add :already_assigned, :boolean, default: false
       add :custom_broker_id, references(:custom_brokers, on_delete: :nothing)
     end
   end
