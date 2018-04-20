10b4d62c65d8ebb1bdb176c55ee0ba73c4d65a60
Xochitl Perez <xochitlperez@Xochitls-iMac.local>
2018-04-20 17:20:36 -0500

    quit warnings

--- apps/ucargo_web/lib/ucargo_web/controllers/record_controller.ex
+++ apps/ucargo_web/lib/ucargo_web/controllers/record_controller.ex
@@ -1,21 +1,8 @@
 defmodule UcargoWeb.RecordController do
   use UcargoWeb, :controller
-  alias Ucargo.Planning
-  alias Ucargo.Order
-  alias Ucargo.Pickup
-  alias Ucargo.Delivery
-  alias Ucargo.Guardian
-  alias Ucargo.CustomBroker
-  alias Ucargo.Bid
-  alias Ucargo.Custom
-  @export 1
-  @import 0
-
 
   def index(conn, _params) do
-    broker = Guardian.Plug.current_resource(conn)
-    plannings = CustomBroker.fetch_plannings(broker)
-    render conn, "index.html", plannings: plannings, broker: broker
+    render conn, "index.html"
   end
 
 end
