997150050a735ed0a57889918cd3e3b9a6575018
Maiku <black.mmms@gmail.com>
2018-06-30 06:26:47 -0500

    The intro controller was modified

--- apps/ucargo_web/lib/ucargo_web/controllers/intro_controller.ex
+++ apps/ucargo_web/lib/ucargo_web/controllers/intro_controller.ex
@@ -7,4 +7,20 @@ defmodule UcargoWeb.IntroController do
     render conn, "index.html"
   end
 
+  def about(conn, _params) do
+    render conn, "about.html"
+  end
+
+  def customs(conn, _params) do
+    render conn, "customs.html"
+  end
+
+  def transporters(conn, _params) do
+    render conn, "transporters.html"
+  end
+
+  def faq(conn, _params) do
+    render conn, "faq.html"
+  end
+
 end
