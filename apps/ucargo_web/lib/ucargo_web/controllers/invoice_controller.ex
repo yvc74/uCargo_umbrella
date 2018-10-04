83ad827b21a36f9885c74833e89eec217e852569
Xochitl Perez <xochitlperez@Xochitls-iMac.local>
2018-10-03 23:32:52 -0500

    add

--- apps/ucargo_web/lib/ucargo_web/controllers/invoice_controller.ex
+++ apps/ucargo_web/lib/ucargo_web/controllers/invoice_controller.ex
@@ -1,11 +1,18 @@
 defmodule UcargoWeb.InvoiceController do
   use UcargoWeb, :controller
+  alias Ucargo.Order
+  alias Ucargo.Guardian
+  alias Ucargo.CustomBroker
+  alias Ucargo.Driver
 
   alias Ucargo.Order
 
-  def index(conn, %{"order_id" => order_id}) do
+  def index(conn, %{"order_id" => order_id, "driver_id" => driver_id}) do
     order = Order.find_by(:id, order_id)
-    render conn, "index.html", section_name: "records", order: order
+    resource = Guardian.Plug.current_resource(conn)
+    driver = Driver.find_by(:id, driver_id)
+    render conn, "index.html", order: order, custom_broker: resource,
+                               planning: order.planning, driver: driver, section_name: "records"
   end
 
   def download_xml(conn, %{"order_id" => order_id}) do
@@ -26,6 +33,6 @@ defmodule UcargoWeb.InvoiceController do
       |> put_resp_header("content-disposition",
                        ~s(attachment; filename="invoice.pdf"))
       |> send_file(200, "/tmp/#{uuid}_invoice.pdf")
-  end
+
 
 end
