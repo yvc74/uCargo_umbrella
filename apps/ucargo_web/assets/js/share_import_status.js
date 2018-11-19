0f9606a2b65435bbfa7ed0d2be12e3b5cac18de8
Misael Pérez Chamorro <misaelpcgm@gmail.com>
2018-11-19 09:15:07 -0600

    Setup Share contents modals for each step

--- apps/ucargo_web/assets/js/share_import_status.js
+++ apps/ucargo_web/assets/js/share_import_status.js
@@ -4,6 +4,7 @@ export class ImportShareStatus {
     this.shareChannel = shareChannel
     this.setupForm()
     this.setupNotifications()
+    this.setupShareContent()
   }
 
   setupForm() {
@@ -11,14 +12,32 @@ export class ImportShareStatus {
     this.roadToCustomStatus = document.querySelector("#roadToCustomStatus")
   }
 
+  setupShareContent() {
+    this.share_modal = $('[data-remodal-id=share-content]').remodal();
+    this.shareTitle = document.querySelector("#shareTitle")
+    this.shareContent = "shareValue"
+    var self = this;
+    $('#shareRoadToCustom').on('click', function(event) {
+      shareTitle.innerHTML = "CAMINO A ADUANA"
+      self.shareContent = "roadToCustom"
+      self.share_modal.open();
+    });
+
+    $('#shareReportLigth').on('click', function(event) {
+      shareTitle.innerHTML = "REPORTE DE SEMÁFORO"
+      self.shareContent = "reportLight"
+      self.share_modal.open();
+    });
+  }
+
   setupNotifications() {
+    var self = this;
     this.shareOnRouteToCustom.onclick = function(){
-      let modal = $('[data-remodal-id=shared]').remodal();
       let emails = $('.input-emails__shared').val();
       let ucargoOrderId = document.querySelector("#ucargoOrderId")
       let shareMyMail = document.querySelector("#checkboxOnRouteToCustom");
-      sendMails(emails, shareMyMail.checked, ucargoOrderId)
-      modal.close();
+      self.sendMails(emails, shareMyMail.checked, ucargoOrderId)
+      self.share_modal.close();
     };
   }
 
