7d6244a7854fd8e45fd8dae5813b5feb840d2c20
Misael Pérez Chamorro <misaelpcgm@gmail.com>
2018-12-03 19:19:34 -0600

    Add history options

--- apps/ucargo_web/lib/ucargo_web/controllers/order_controller.ex
+++ apps/ucargo_web/lib/ucargo_web/controllers/order_controller.ex
@@ -28,9 +28,17 @@ defmodule UcargoWeb.OrderController do
       |> render("orders.json", %{orders: updated_driver.assigned_orders})
   end
 
-  def show(conn, _params) do
+  def show(conn, %{"status" => "history"}) do
     driver = conn.assigns[:driver]
     available_orders = AvailableOrder.find_completed_by(driver.id)
+    conn
+      |> put_status(200)
+      |> render("available_orders.json", %{orders: available_orders})
+  end
+
+  def show(conn, _params) do
+    driver = conn.assigns[:driver]
+    available_orders = AvailableOrder.find_by(driver.id)
     conn
       |> put_status(200)
       |> render("available_orders.json", %{orders: available_orders})
