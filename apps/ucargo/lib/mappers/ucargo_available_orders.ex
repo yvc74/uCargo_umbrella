8ce42ab59ad9b5027b34f52001e90ea9ccd5e3e7
Misael Pérez Chamorro <misaelpcgm@gmail.com>
2018-12-19 10:12:31 -0600

    add query for aproveed, tha does not include new, and finished

--- apps/ucargo/lib/mappers/ucargo_available_orders.ex
+++ apps/ucargo/lib/mappers/ucargo_available_orders.ex
@@ -38,6 +38,13 @@ defmodule Ucargo.AvailableOrder do
     Repo.all(query)
   end
 
+  def find_approved(driver_id) do
+    query = from a in AvailableOrder,
+            where: a.driver_id == ^driver_id and a.status != "New" and a.status != "Signed" and a.status != "Finished",
+            preload: [:bid, [order: [:pickup, [available_order: :bid], :delivery, :custom, [planning: [:custom_broker, auction: [:bids]]]]]]
+    Repo.all(query)
+  end
+
   def find_completed_by(driver_id) do
     query = from a in AvailableOrder,
             where: a.driver_id == ^driver_id and a.status == "Signed" or a.status == "Finished",
--- apps/ucargo_web/lib/ucargo_web/controllers/order_controller.ex
+++ apps/ucargo_web/lib/ucargo_web/controllers/order_controller.ex
@@ -30,10 +30,10 @@ defmodule UcargoWeb.OrderController do
 
   def show(conn, %{"status" => "approved"}) do
     driver = conn.assigns[:driver]
-    updated_driver = Driver.get_approved(driver)
+    available_orders = AvailableOrder.find_approved(driver.id)
     conn
       |> put_status(200)
-      |> render("orders.json", %{orders: updated_driver.assigned_orders})
+      |> render("available_orders.json", %{orders: available_orders})
   end
 
   def show(conn, %{"status" => "history"}) do
